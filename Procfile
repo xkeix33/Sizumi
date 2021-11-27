99bacb3cdfaedf0df6291467a32314caf4c1f981
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-11-27 23:40:16 +0500

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m RocksRobot
+web: python3 -m RocksRobot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE RocksRobot Chan
+:: Enables virtual env mode and then starts Vegeta
+env\scripts\activate.bat && py -m RocksRobot
