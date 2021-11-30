a89c57ae75a8087489a45e788a7a588a90094e1f
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-11-30 07:28:50 +0500

    Update blacklistusers.py

--- RocksAlexaRobot/modules/blacklistusers.py
+++ RocksAlexaRobot/modules/blacklistusers.py
@@ -164,5 +164,5 @@ dispatcher.add_handler(BL_HANDLER)
 dispatcher.add_handler(UNBL_HANDLER)
 dispatcher.add_handler(BLUSERS_HANDLER)
 
-__mod_name__ = "Blacklisting Users"
+__mod_name__ = "🚧 ʙʟᴀᴄᴋʟɪsᴛɪɴɢ ᴜsᴇʀs"
 __handlers__ = [BL_HANDLER, UNBL_HANDLER, BLUSERS_HANDLER]
