ca1a4370dc9644a26e11b8b2e2807fcf044b9695
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-11-30 10:02:18 +0500

    Update fun_strings.py

--- RocksAlexaRobot/modules/fun_strings.py
+++ RocksAlexaRobot/modules/fun_strings.py
@@ -257,7 +257,21 @@ SFW_STRINGS = (
 )
     
   
-REPO = """[OWNER](t.me/Dr_Asad_Ali) - [YOUTUBE](youtube.com/jankarikiduniya) - [MY BRO](t.me/HarshitSharma361) - [HEART ME](t.me/Give_Me_Heart)"""
+REPO = """━━━━━━━━━━━━━━━━━━━━━━━━
+💥 A ᴘᴏᴡᴇʀғᴜʟ ɢʀᴏᴜᴘ  ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ʙᴏᴛ
+ᴏғ ♻️ ᴅʀ ᴀsᴀᴅ ᴀʟɪ 🔥
+━━━━━━━━━━━━━━━━━
+ʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘs ᴍᴀɴᴀɢᴇᴍᴇɴᴛ...
+┏━━━━━━━━━━━━━━━━━┓
+┣★ [𝐂𝐫𝐞𝐚𝐭𝐨𝐫] @Dr_Asad_Ali
+┣★ [𝐇𝐞𝐚𝐫𝐭]     @Give_Me_Heart
+┣★ [𝐁𝐨𝐭 𝐔𝐩𝐝𝐚𝐭𝐞𝐬] @AsadSupport)
+┣★ [𝐎𝐮𝐫 𝐅𝐞𝐝] @Part_Of_Rocks)
+┣★ [𝐆𝐫𝐨𝐮𝐩] @Shayri_Music_Lovers)
+┗━━━━━━━━━━━━━━━━━┛
+
+💞 
+IF HAVE ANY QUESTION OR WANT REPO THEN CONTACT » TO » MY » [OWNER] @Dr_Asad_Ali)"""
 
 
   
