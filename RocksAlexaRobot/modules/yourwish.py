20c082b3b87e25c1f9a36d2d97ec72aefb57ac87
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-12-03 16:19:55 +0500

    Update yourwish.py

--- RocksAlexaRobot/modules/yourwish.py
+++ RocksAlexaRobot/modules/yourwish.py
@@ -16,20 +16,20 @@ async def wish(alexa):
          await tbot.send_message(alexa.chat_id, f"**Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=lol)
    if not alexa.is_reply:
          mm = random.randint(1,100)
-         ALEXA = "https://telegra.ph/file/68be46bb292a9230a584b.jpg"
+         ALEXA = "https://telegra.ph/file/6760471209d90bcad8b1f.jpg"
          await tbot.send_file(alexa.chat_id, ALEXA,caption=f"**Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=alexa)
          lol = await alexa.get_reply_message()
          await tbot.send_file(alexa.chat_id, f"**Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=lol, file=ALEXA)
    if not alexa.is_reply:
          mm = random.randint(1,100)
-         ALEXA = "https://telegra.ph/file/68be46bb292a9230a584b.jpg"
+         ALEXA = "https://telegra.ph/file/6760471209d90bcad8b1f.jpg"
          await tbot.send_file(alexa.chat_id,f"**Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=lol, file=ALEXA)
          await tbot.send_file(alexa.chat_id, ALEXA,caption=f"**Your wish has been cast.✨**\n__chance of success {mm}%__", reply_to=alexa)
          lol = await alexa.get_reply_message()
          await tbot.send_file(alexa.chat_id, f"**Your wish has been cast.✨**\n__chance of success {mm}%__", reply_to=lol)
    if not alexa.is_reply:
          mm = random.randint(1,100)
-         ALEXA = "https://telegra.ph/file/68be46bb292a9230a584b.jpg"
+         ALEXA = "https://telegra.ph/file/6760471209d90bcad8b1f.jpg"
          await tbot.send_file(alexa.chat_id, ALEXA,caption=f"**Your wish has been cast.✨**\n__chance of success {mm}%__", reply_to=lol,file=alexa)
 
         
