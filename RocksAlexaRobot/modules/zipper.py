070af0341eaef0c4c57938712f791bdad10b7941
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-12-05 15:46:10 +0500

    Ittu 🤏 Sa

--- RocksAlexaRobot/modules/zipper.py
+++ RocksAlexaRobot/modules/zipper.py
@@ -2,7 +2,7 @@
 # Property Of Rocks Indian Largest Chatting Group
 # Without Credit (Mother Fucker)
 # Rocks © @Dr_Asad_Ali © Rocks
-# Owner Asad + Harshit
+# Owner Asad Ali + Harshit
 # Roses are red, Violets are blue, A face like yours, Belongs in a zoo
 
 
