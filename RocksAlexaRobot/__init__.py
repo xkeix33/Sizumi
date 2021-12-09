79162ee7f2e8194aa772ce46c81722538136e203
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-12-09 17:42:43 +0500

    Ittu 🤏 Sa

--- RocksAlexaRobot/__init__.py
+++ RocksAlexaRobot/__init__.py
@@ -81,6 +81,7 @@ if ENV:
     URL = os.environ.get('URL', "")  # Does not contain token
     PORT = int(os.environ.get('PORT', 5000))
     CERT_PATH = os.environ.get("CERT_PATH")
+    REDIS_URL = os.environ.get('REDIS_URL')
     API_ID = os.environ.get('API_ID', None)
     API_HASH = os.environ.get('API_HASH', None)
     DB_URI = os.environ.get('DATABASE_URL')
@@ -163,6 +164,7 @@ else:
     URL = Config.URL
     PORT = Config.PORT
     CERT_PATH = Config.CERT_PATH
+    REDIS_URL = os.environ.get('REDIS_URL')
     API_ID = Config.API_ID
     API_HASH = Config.API_HASH
 
